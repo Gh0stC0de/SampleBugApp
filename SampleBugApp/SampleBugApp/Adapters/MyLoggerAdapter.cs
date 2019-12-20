0512d2b3145f6351eccde818759e00b137cbc6ce
Gh0stC0de <aaron.mueller@sunrise.ch>
2019-12-20 09:26:40 +0100

    Add project.

---
+++ SampleBugApp/SampleBugApp/Adapters/MyLoggerAdapter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Prism.Logging;
+
+namespace SampleBugApp.Adapters
+{
+    public class MyLoggerAdapter : ILoggerFacade
+    {
+        public void Log(string message, Category category, Priority priority)
+        {
+            Console.WriteLine(
+                $"{Enum.GetName(typeof(Category), category)}|{Enum.GetName(typeof(Priority), priority)}|{message}");
+        }
+    }
+}
---
+++ SampleBugApp/SampleBugApp/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Logging;
+using SampleBugApp.Adapters;
+using SampleBugApp.ViewModels;
+using SampleBugApp.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace SampleBugApp
+{
+    public partial class App
+    {
+        /* 
+         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+         * This imposes a limitation in which the App class must have a default constructor. 
+         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+         */
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.Register<ILoggerFacade, MyLoggerAdapter>();
+
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+            containerRegistry.RegisterForNavigation<PrismTabbedPage1, PrismTabbedPage1ViewModel>();
+        }
+    }
+}
